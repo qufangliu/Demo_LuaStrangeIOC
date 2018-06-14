6ba319f4d8b6962eb7e8e1befaf8b8d1341dc8e8
qufangliu <qufangliu@qufangliudeMac-mini.local>
2018-06-14 14:43:02 +0800

    接入tolua
    1.拷贝tolua/Editor/Custom到myproj/Editor/ToLua/Custom
    2.拷贝tolua/Plugins到myproj/Plugins/ToLua
    3.拷贝tolua/Source到myproj/Source
    4.拷贝tolua/ToLua到myproj/ToLua

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// cs test
+		Debug.Log( "Game Start!!" );
+		
+		// tolua test
+		string luaStr = "print('log in lua!!')";
+		LuaState luaState = new LuaState();
+		luaState.Start();
+		luaState.DoString( luaStr );
+		luaState.Dispose();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
